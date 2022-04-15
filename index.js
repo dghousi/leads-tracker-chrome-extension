8fff9df22fa08ea58a66c75d20b5692a8bac9e2d
dawlatzai <dawlatzai.ghousi@gmail.com>
2022-04-15 12:41:13 +0430

    Add tab functionality that get url from the current active tab

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 let myLeads = []
-let oldLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById('delete-btn')
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+const tabBtn = document.getElementById('tab-btn')
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
 
-function render(t) {
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
+
+function render(leads) {
   let listItems = ''
   for (let i = 0; i < leads.length; i++) {
     listItems += `
